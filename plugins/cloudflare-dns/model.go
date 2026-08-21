5452bf279fb06e3dac8707f6654ce215fb7f21f4
sakullla <sakullla@yahe.online>
2026-08-21 21:41:32 +0800

    fix(cloudflare-dns): rotate invalid mapped tokens

--- plugins/cloudflare-dns/model.go
+++ plugins/cloudflare-dns/model.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	PluginID       = "cloudflare-dns"
-	PluginVersion  = "0.1.10"
+	PluginVersion  = "0.1.11"
 	MaxConfigBytes = 1 << 20
 	MaxZones       = 256
 	MaxRecords     = 1024
