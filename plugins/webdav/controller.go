330ba0fe687a1da4f89894a10602c2dd54ffa661
sakullla <sakullla@yahe.online>
2026-08-23 20:08:17 +0800

    1

--- plugins/webdav/controller.go
+++ plugins/webdav/controller.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	PluginID         = "webdav"
-	PluginVersion    = "0.1.1"
+	PluginVersion    = "0.1.2"
 	ProviderID       = "default"
 	MaxConfigBytes   = 4096
 	MaxPasswordBytes = 256
