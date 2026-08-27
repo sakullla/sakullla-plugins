169659f739caeb8e6779bd585f57647e42c456cf
sakullla <sakullla@yahe.online>
2026-08-27 08:36:31 +0800

    chore(docker-app): bump version to 0.1.25
    Republish the list/detail admin workspace after 0.1.24.

--- plugins/docker-app/model.go
+++ plugins/docker-app/model.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	PluginID                 = "docker-app"
-	PluginVersion            = "0.1.24"
+	PluginVersion            = "0.1.25"
 	DeclaredResourceGroupRef = "resource-group/docker-app"
 	MaxApps                  = 128
 	MaxDiscoveries           = 512
