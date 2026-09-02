17c283f6af6a448c116ef88462a6b8cf154c9982
sakullla <sakullla@yahe.online>
2026-09-02 22:58:54 +0800

    chore(accelerator-sources): bump version to 0.1.10

--- plugins/accelerator-sources/controller.go
+++ plugins/accelerator-sources/controller.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	PluginID       = "accelerator-sources"
-	PluginVersion  = "0.1.9"
+	PluginVersion  = "0.1.10"
 	ProviderID     = "default"
 	MaxConfigBytes = 4096
 	maxSources     = 32
