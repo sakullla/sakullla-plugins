5e654ce437ea4a6bfefca202e922e04d99490d87
sakullla <sakullla@yahe.online>
2026-09-02 22:22:17 +0800

    fix(shadowsocks-server): return only granted capabilities

--- plugins/shadowsocks-server/model.go
+++ plugins/shadowsocks-server/model.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	PluginID             = "shadowsocks-server"
-	PluginVersion        = "0.1.8"
+	PluginVersion        = "0.1.9"
 	MaxConfigBytes       = 1 << 20
 	MaxListeners         = 256
 	MaxUsers             = 256
