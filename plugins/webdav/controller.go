6db629f0c1259084f225b9d8334c2e22f0c1361e
sakullla <sakullla@yahe.online>
2026-09-05 23:33:54 +0800

    chore(webdav): bump version to 0.1.9

--- plugins/webdav/controller.go
+++ plugins/webdav/controller.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	PluginID         = "webdav"
-	PluginVersion    = "0.1.8"
+	PluginVersion    = "0.1.9"
 	ProviderID       = "default"
 	MaxConfigBytes   = 4096
 	MaxPasswordBytes = 256
