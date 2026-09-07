ecb39e6ecaed3e2a0388bccde4a018198cbef320
sakullla <sakullla@yahe.online>
2026-09-07 22:52:20 +0800

    chore(docker-app): bump version to 0.1.44

--- plugins/docker-app/model.go
+++ plugins/docker-app/model.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	PluginID                 = "docker-app"
-	PluginVersion            = "0.1.43"
+	PluginVersion            = "0.1.44"
 	DeclaredResourceGroupRef = "resource-group/docker-app"
 	MaxApps                  = 128
 	MaxDiscoveries           = 512
