98c7d4c9ec7c6c83188e075e6ad869405f280a87
sakullla <sakullla@yahe.online>
2026-09-19 10:24:54 +0800

    chore(reverse-l4): sync PluginVersion 0.3.9 and rebind artifact pin
    Sakullla-Task: release-metadata
    Sakullla-Attempt: release-metadata-A2-17a99d6cc43aa2be

--- plugins/reverse-l4/model.go
+++ plugins/reverse-l4/model.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	PluginID                 = "reverse-l4"
-	PluginVersion            = "0.3.8"
+	PluginVersion            = "0.3.9"
 	DeclaredResourceGroupRef = "resource-group/reverse-l4"
 
 	// MaxMappings bounds the durable mapping catalog.
